ffff472bdfe86f13074f2839075a2814a5e7aeaf
asiaziola <ziola.jm@gmail.com>
2022-07-04 11:37:18 +0200

    asiaziola/arname-warp

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
-import React from "react";
-import { warp } from "./utils/config";
-import * as fdd from "./HllvB064_PZSaeoXj4gtX_wdj22WawoyAz5iCuq1uzs.json";
+import React, { useEffect, useState } from 'react';
+import { arweave, warp } from './utils/config';
+import * as fdd from './HllvB064_PZSaeoXj4gtX_wdj22WawoyAz5iCuq1uzs.json';
+import { mineBlock } from './utils/mineBlock';
 const App = () => {
-  const contract = warp
-    .contract("DsHwq-Y4Lwvik3Wc2kxKawS_wpsIDAtadEbCklqTpvM")
-    .connect(fdd);
-  contract
-    .writeInteraction({ function: "register", text: "memCached" })
-    .then()
-    .catch((err) => console.log(err));
-  contract.readState().then((e) => console.log(e.state));
+  const contract = warp.contract('DsHwq-Y4Lwvik3Wc2kxKawS_wpsIDAtadEbCklqTpvM').connect(fdd);
+  const [contractState, setContractState] = useState({});
+  const [inputValue, setInputValue] = useState('');
+  const handleInputChange = (e: any) => {
+    setInputValue(e.target.value);
+  };
+  const handleClickButton = async () => {
+    const txId = await contract.writeInteraction({ function: 'register', text: inputValue });
+    console.log(txId);
+    await mineBlock(arweave);
+    await fetchContractData();
+    setInputValue('');
+  };
+  async function fetchContractData() {
+    const result = await contract.readState();
+    setContractState(result);
+  }
+  useEffect(() => {
+    fetchContractData();
+  }, []);
+
   return (
     <>
-      <h1>dsf</h1>
+      <div>{JSON.stringify(contractState)}</div>
+      <div>
+        <input value={inputValue} onChange={handleInputChange} />
+        <button onClick={handleClickButton}>Register</button>
+      </div>
     </>
   );
 };
